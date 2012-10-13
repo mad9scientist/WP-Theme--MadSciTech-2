345e70651992269fc98b6a96a157507526fce05d
Chris Holbrook <chris@madscitech.com>
2012-10-13 05:35:37 -0500

    Commented out the Old Live Chat Shit from the primary JS file.

--- js/madscitech.js
+++ js/madscitech.js
@@ -93,7 +93,7 @@ $("#E404")
 
 
 // Live Chat Shit
-function livechat()
+/*function livechat()
 {
 	if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('http://chat.madscitech.com/client.php?locale=en', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=740,height=800,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;
 }
@@ -102,7 +102,7 @@ function livechat()
 $(document).ready(function() {
 	$("a:[title='Mad Sci Tech Chat Support']").attr('onclick', 'livechat()');
 });
-
+*/
 $(document).ready(function(){
     $("#thumbNav,#start-stop").fadeOut();
 });  
