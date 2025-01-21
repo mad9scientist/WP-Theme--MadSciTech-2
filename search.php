9e5a1e9a4964689d64e15b4936ad801a2ad4dec3
Chris Holbrook <chris@madscitech.com>
2025-01-21 17:45:09 -0600

    Fixed Layout/Headers on Search

--- search.php
+++ search.php
@@ -1,28 +1,36 @@
 <?php
 get_header(); ?>
 <div id="articles">
-	<?php if (have_posts()) : ?>
-		<h2>Search Results</h2>
-		<div class="article">
-		<?php while (have_posts()) : the_post(); ?>
-			<div class="post-entry">
-				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-				<?php the_time('l, F jS, Y') ?>
-				<!--<p><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>-->
-			</div>
-		<?php endwhile; ?>
-
-		<div class="center"><p><?php next_posts_link('&laquo; Older Articles') ?>  <?php previous_posts_link('Newer Articles &raquo;') ?></p></div>
-
-	<?php else : ?>
-
+	<h2>Search Results</h2>
 	<div class="article">
-		<h2>No posts found. Try a different search?</h2>
-        <div id="search-field-page">
+		<div id="search-field-page">
 			<?php get_search_form(); ?>
 		</div>
-	<?php endif; ?>
+<?php if (have_posts()) : ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<div class="post-entry">
+			<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+			Posted: <?php the_time('l, F jS, Y') ?>
+			<?php /*<p>
+				<?php the_tags('Tags: ', ', ', '<br />'); ?> 
+				Posted in <?php the_category(', ') ?> | 
+				<?php edit_post_link('Edit', '', ' | '); ?>  
+				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+			</p> */ ?>
+		</div>
+<?php endwhile; ?>
+	<div class="center">
+		<p><?php next_posts_link('&laquo; Older Articles') ?> &bull; <?php previous_posts_link('Newer Articles &raquo;') ?></p>
+	</div>
 
-</div></div>
+<?php else : ?>
+	<h2>No posts found. Try a different search?</h2>
+        <div id="search-field-page">
+		<?php get_search_form(); ?>
+	</div>
+<?php endif; ?>
+
+	</div> <!-- End of .article -->
+</div> <!-- End of .articles -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
