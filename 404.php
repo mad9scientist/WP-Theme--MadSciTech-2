9164ed48d60a837be0ca45e0a7d74e9b671c5290
Chris Holbrook <chris@madscitech.com>
2025-01-21 17:59:24 -0600

    Update 404.php
    Dropped the Google Site Search for the builtin search field. Reformatted page layout and update language

--- 404.php
+++ 404.php
@@ -15,23 +15,25 @@ get_header();
 <div id="articles">
 	<div class="article">
 		<div style="text-align:center">
-        	<h2><i>Uh Oh! We Broke Something!</i><br />404 - Resource Not Found</h2>
-            <br /><br />Requested Page: <?php echo $page; ?><br />
-<br />
-
-			We're sorry, the page you requested could not be found. <br />You may have received this error by following an outdated link or mistyped the address.<br />
-<br />                   
-            <form action="/search/" id="cse-search-box">
-                      <div>
-                        <input type="hidden" name="cx" value="015463815136209645922:ojuahnyh9fo" />
-                        <input type="hidden" name="cof" value="FORID:9" />
-                        <input type="hidden" name="ie" value="UTF-8" />
-                        <input type="text" name="q" size="20" value="Search..." id="E404" />
-                        <input type="button" name="sa" value="Search" />
-                      </div>
-                    </form>
-                    <br /><br />
-<br />
+        	<h2><i>Uh Oh! We Broke Something!</i><br />HTTP/404 - Resource Not Found</h2>
+            	<p>Requested Page: <?php echo $page; ?></p>
+		<p></p>We're sorry, the page you requested could not be found. <br />You have received this error by following an outdated link, a mistyped address or we broke something.</p>
+		<br /><br />
+			<h3>Try Searching?</h3>
+		<?php 
+			get_search_form()
+            	/* Old unused Google Search
+	     	<form action="/search/" id="cse-search-box">
+                	<div>
+                        	<input type="hidden" name="cx" value="015463815136209645922:ojuahnyh9fo" />
+                        	<input type="hidden" name="cof" value="FORID:9" />
+                        	<input type="hidden" name="ie" value="UTF-8" />
+                        	<input type="text" name="q" size="20" value="Search..." id="E404" />
+                        	<input type="button" name="sa" value="Search" />
+                	</div>
+                </form> */
+		?>
+		<br /><br /><br />
 <?php
 // If a user lands on the 404 page more than once in a five minute period
 // display contact information
