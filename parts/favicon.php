e124b7c456922ee94d1767f1cb024219bfad243e
Chris Holbrook <chris@madscitech.com>
2025-05-30 18:03:17 -0500

    Update favicon.php
    Cache Busting and Adding more favicon for May 2025 Updated icons

--- parts/favicon.php
+++ parts/favicon.php
@@ -10,14 +10,16 @@
 <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=gAAAK6Gzll">
 <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=gAAAK6Gzll">
 <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=gAAAK6Gzll">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=gAAAK6Gzll">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=20250530" />
 <link rel="icon" type="image/png" href="/favicon-32x32.png?v=gAAAK6Gzll" sizes="32x32">
 <link rel="icon" type="image/png" href="/favicon-194x194.png?v=gAAAK6Gzll" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicon-96x96.png?v=gAAAK6Gzll" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-96x96.png?v=20250530" sizes="96x96">
 <link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=gAAAK6Gzll" sizes="192x192">
 <link rel="icon" type="image/png" href="/favicon-16x16.png?v=gAAAK6Gzll" sizes="16x16">
+<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=20250530" />
 <link rel="manifest" href="/manifest.json?v=gAAAK6Gzll">
-<link rel="shortcut icon" href="/favicon.ico?v=gAAAK6Gzll">
+<link rel="manifest" href="/site.webmanifest?v=20250530" />
+<link rel="shortcut icon" href="/favicon.ico?v=20250530">
 <meta name="apple-mobile-web-app-title" content="MadSciTech">
 <meta name="application-name" content="MadSciTech">
 <meta name="msapplication-TileColor" content="#0099ff">
